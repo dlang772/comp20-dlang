0c407d15d6d7c4253c0e1536ae54bd12da3c7c80
Daniel Lang <daniellang@Daniels-MBP-8.T-mobile.com>
2016-07-27 21:58:59 -0400

    Updated script with locations

--- mbta/script.js
+++ mbta/script.js
@@ -1,60 +1,54 @@
 var mapCanvas = document.getElementById("map");
 var mapOptions = {
-    center: new google.maps.LatLng(42.352271, -71.05524200000001), zoom: 25
+    center: new google.maps.LatLng(42.352271, -71.05524200000001), zoom: 11
 }
 var map = new google.maps.Map(mapCanvas, mapOptions);
 
+var RedlineCoordinates1 = [
+	{lat: 42.395428, lng: -71.142483},
+	{lat: 42.39674, lng: -71.121815},
+    {lat: 42.3884, lng: -71.11914899999999},
+    {lat: 42.373362, lng: -71.118956},
+    {lat: 42.365486, lng: -71.103802},
+    {lat: 42.36249079, lng: -71.08617653},
+    {lat: 42.361166, lng:  -71.070628},
+    {lat: 42.35639457, lng: -71.0624242},
+    {lat: 42.355518, lng: -71.060225},
+	{lat: 42.352271, lng: -71.05524200000001},
+    {lat: 42.342622, lng: -71.056967},
+    {lat: 42.330154, lng: -71.057655},
+    {lat: 42.320685, lng: -71.052391},
+    {lat: 42.275275, lng: -71.0203369},
+    {lat: 42.2665139, lng: -71.029583},
+    {lat: 42.251809, lng: -71.005409},
+    {lat: 42.233391, lng: -71.007153},
+    {lat: 42.2078543, lng:  -71.0011385}
+]
+   
+var RedlineCoordinates2 = [
+	{lat: 42.320685, lng: -71.052391},
+	{lat: 42.31129, lng: -71.053331},
+	{lat: 42.300093, lng: -71.061667},
+	{lat: 42.29312583, lng: -71.06573796000001},
+	{lat: 42.284652, lng: -71.06448899999999},
+]
+
+var Redline1 = new google.maps.Polyline({
+	path: RedlineCoordinates1,
+	geodesic: true,
+	strokeColor: '#FF0000',
+	strokeOpacity: 1.0,
+	strokeWeight: 2
+});
+
+var Redline2 = new google.maps.Polyline({
+	path: RedlineCoordinates2,
+	geodesic: true,
+	strokeColor: '#FF0000',
+	strokeOpacity: 1.0,
+	strokeWeight: 2
+});
+
+Redline1.setMap(map)+Redline2.setMap(map);
 
 
-// var myLat = 0;
-// 			var myLng = 0;
-// 			var request = new XMLHttpRequest();
-// 			var me = new google.maps.LatLng(myLat, myLng);
-// 			var myOptions = {
-// 						zoom: 13, // The larger the zoom number, the bigger the zoom
-// 						center: me,
-// 						mapTypeId: google.maps.MapTypeId.ROADMAP
-// 					};
-// 			var map;
-// 			var marker;
-// 			var infowindow = new google.maps.InfoWindow();
-			
-// 			function init()
-// 			{
-// 				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-// 				getMyLocation();
-// 			}
-			
-// 			function getMyLocation() {
-// 				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-// 					navigator.geolocation.getCurrentPosition(function(position) {
-// 						myLat = position.coords.latitude;
-// 						myLng = position.coords.longitude;
-// 						renderMap();
-// 					});
-// 				}
-// 				else {
-// 					alert("Geolocation is not supported by your web browser.  What a shame!");
-// 				}
-// 			}
-
-// 			function renderMap()
-// 			{
-// 				me = new google.maps.LatLng(myLat, myLng);
-				
-// 				// Update map and go there...
-// 				map.panTo(me);
-	
-// 				// Create a marker
-// 				marker = new google.maps.Marker({
-// 					position: me,
-// 					title: "Here I Am!"
-// 				});
-// 				marker.setMap(map);
-					
-// 				// Open info window on click of marker
-// 				google.maps.event.addListener(marker, 'click', function() {
-// 					infowindow.setContent(marker.title);
-// 					infowindow.open(map, marker);
-// 				});
-// 			}
