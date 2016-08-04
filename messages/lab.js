9478559f19ce23727ef1a352c1f00581c1e91b85
Daniel Lang <daniellang@Daniels-MBP-8.T-mobile.com>
2016-08-03 20:10:40 -0400

    Modified script with xmlhttprequest

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here...
+
+function parse(){
+	var xmlhttp = new XMLHttpRequest();
+	var url = "data.json";
+
+	xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var parseData = JSON.parse(xmlhttp.responseText);
+        printData(parseData);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+}
+
+function printData(parseData) {
+        
+        var chat = "";
+
+        for (i in parseData) {
+        chat += parseData[i].content + ' ' + parseData[i].username + "<br/>";
+        }
+        document.getElementById("messages").innerHTML = chat;
+}
